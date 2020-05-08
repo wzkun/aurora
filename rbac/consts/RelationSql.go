802e8a47e7cd8f9cbd3b07e18b504fa1e19a26fb
wzkun <wzkun1990@126.com>
2020-05-08 18:37:27 +0800

    修改relationsql

--- rbac/consts/RelationSql.go
+++ rbac/consts/RelationSql.go
@@ -1,43 +1,84 @@
 package consts
 
 const (
-	QueryRoleDetail = `
+	QueryRoleBrief = `
 		select a.roleid as id, b.name as name 
 		from rbac_server_userrolerelation a 
 		left join rbac_server_role b 
 		on a.roleid = b.id 
 		where a.account = %s 
-`
+	`
+
+	QueryRoleDetail = `
+		select b.* 
+		from rbac_server_userrolerelation a 
+		left join rbac_server_role b 
+		on a.roleid = b.id 
+		where a.account = %s 
+	`
+
+	QueryOrganizationBrief = `
+		select a.orgid as id, b.name ad name 
+		from rbac_server_userorganizationrelation a 
+		left join rbac_server_organization b 
+		on a.orgid = b.id 
+		where a.account = %s 
+	`
 
 	QueryOrganizationDetail = `
-	select a.orgid as id, b.name ad name 
-	from rbac_server_userorganizationrelation a 
-	left join rbac_server_organization b 
-	on a.orgid = b.id 
-	where a.account = %s 
-`
+		select b.* 
+		from rbac_server_userorganizationrelation a 
+		left join rbac_server_organization b 
+		on a.orgid = b.id 
+		where a.account = %s 
+	`
+
+	QueryMenuBrief = `
+		select a.menuid as id, b.name ad name 
+		from rbac_server_usermenurelation a 
+		left join rbac_server_menu b 
+		on a.menuid = b.id 
+		where a.account = %s 
+	`
 
 	QueryMenuDetail = `
-	select a.menuid as id, b.name ad name 
-	from rbac_server_usermenurelation a 
-	left join rbac_server_menu b 
-	on a.menuid = b.id 
-	where a.account = %s 
-`
+		select b.* 
+		from rbac_server_usermenurelation a 
+		left join rbac_server_menu b 
+		on a.menuid = b.id 
+		where a.account = %s 
+	`
+
+	QueryGroupBrief = `
+		select a.groupid as id, b.name ad name 
+		from rbac_server_usergrouprelation a 
+		left join rbac_server_group b 
+		on a.groupid = b.id 
+		where a.account = %s 
+	`
 
 	QueryGroupDetail = `
-	select a.groupid as id, b.name ad name 
-	from rbac_server_usergrouprelation a 
-	left join rbac_server_group b 
-	on a.groupid = b.id 
-	where a.account = %s 
-`
+		select b.* 
+		from rbac_server_usergrouprelation a 
+		left join rbac_server_group b 
+		on a.groupid = b.id 
+		where a.account = %s 
+	`
 
+	QueryFunctionBrief = `
+		select a.funcid as id, b.name ad name 
+		from rbac_server_userfunctionrelation a 
+		left join rbac_server_function b 
+		on a.funcid = b.id 
+		where a.account = %s 
+	`
+	
 	QueryFunctionDetail = `
-	select a.funcid as id, b.name ad name 
+	select b.* 
 	from rbac_server_userfunctionrelation a 
 	left join rbac_server_function b 
 	on a.funcid = b.id 
 	where a.account = %s 
 `
+
 )
