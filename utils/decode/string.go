3cb65977c6f43a37bb4ceb6865e279063419b954
wzkun <wzkun1990@126.com>
2020-05-09 15:59:11 +0800

    增加对象转换为字符串函数

--- utils/decode/string.go
+++ utils/decode/string.go
@@ -1,5 +1,6 @@
 package decode
 
+// ToStringArray function.
 func ToStringArray(objs ...interface{}) []string {
 	records := make([]string, 0, len(objs))
 	for _, obj := range objs {
@@ -8,4 +9,11 @@ func ToStringArray(objs ...interface{}) []string {
 		records = append(records, record)
 	}
 	return records
+}
+
+// ToString function.
+func ToString(obj interface{}) string {
+	js, _ := JSON.Marshal(obj)
+	record := string(js)
+	return record
 }
