67ee9b9eae71085293df10232bdb51282f9c34e7
wzkun <wzkun1990@126.com>
2020-05-12 10:57:33 +0800

    增加生成rsa函数，获取公钥和私钥函数

--- utils/decode/rsabase64.go
+++ utils/decode/rsabase64.go
@@ -3,7 +3,12 @@ package decode
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"io/ioutil"
+	"os"
 )
 
 // 编码
@@ -59,4 +64,91 @@ func DecodeRSA(prikey *rsa.PrivateKey, data string) ([]byte, error) {
 	}
 
 	return dsignature, nil
+}
+
+// GetPubKey function
+func GetPubKey(filename string) (*rsa.PublicKey, error) {
+	PubKey, err := ioutil.ReadFile(filename)
+	if err != nil {
+		os.Exit(-1)
+	}
+
+	if PubKey == nil {
+		return nil, errors.New("input arguments error")
+	}
+
+	block, _ := pem.Decode(PubKey)
+	if block == nil {
+		return nil, errors.New("public rsaKey error")
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+
+	return pub, nil
+}
+
+// GetPriKey function.
+func GetPriKey(filename string) (*rsa.PrivateKey, error) {
+	PriKey, err := ioutil.ReadFile(filename)
+	if err != nil {
+		os.Exit(-1)
+	}
+	if PriKey == nil {
+		return nil, errors.New("input arguments error")
+	}
+
+	block, _ := pem.Decode(PriKey)
+	if block == nil {
+		return nil, errors.New("private rsaKey error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+// GenRsaKey function.
+func GenRsaKey(bits int) error {
+	// 生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("config/pri.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	// 生成公钥文件
+	publicKey := &privateKey.PublicKey
+	derPkix, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+	file, err = os.Create("config/pub.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	return nil
 }
