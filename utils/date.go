fbcc879c33c29fae88ccb504f05c57e86756546e
wzkun <wzkun1990@126.com>
2020-07-09 11:54:12 +0800

    添加获取时间戳的字符串作为id的接口

--- utils/date.go
+++ utils/date.go
@@ -1,16 +1,32 @@
 package utils
 
-import "time"
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 const (
 	YYYY_MM_DD          = "2006-01-02"
 	DATE_DIR_PATTERN    = "2006/01/02"
 	YYYY_MM_DD_HH_MM_SS = "2006-01-02 15:04:05"
+	YYYYMMDDHHMMSS      = "20060102150405"
 )
 
 // FormatDateToString 将日期转换成指定格式的字符串
 func FormatDateToString(date time.Time, format string) string {
 	return date.Format(format)
+}
+
+// GetTimeStampStringId 获取时间戳的字符串作为id
+func GetTimeStampStringId(date time.Time) string {
+	ds := date.Format(YYYYMMDDHHMMSS)
+	Nanosecond := date.Nanosecond()
+	rd := rand.Intn(10000)
+
+	id := ds + strconv.Itoa(Nanosecond) + strconv.Itoa(rd)
+
+	return id
 }
 
 // ParseStringToDate 将字符串日期转换成日期
