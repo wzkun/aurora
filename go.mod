d9772d787b8d9f2cae75aeb697e0d7a77883dde4
wzkun <wzkun1990@126.com>
2020-08-06 15:53:12 +0800

    增加转弹性搜索数据的接口

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/wzkun/aurora
 go 1.14
 
 require (
+	code.aliyun.com/new_backend/scodi_nqc v0.0.0-20200805031749-f52e74c01c2c
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/json-iterator/go v1.1.10
+	github.com/mailru/easyjson v0.7.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20200707235045-ab33eee955e0
+	github.com/olivere/elastic/v7 v7.0.19 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208 // indirect
+	golang.org/x/sys v0.0.0-20200806060901-a37d78b92225 // indirect
 )
---
+++ utils/Interface.go
@@ -0,0 +1,45 @@
+package utils
+
+import "code.aliyun.com/new_backend/scodi_nqc/elastic"
+
+type Item interface {
+	Idx() string
+	ElasticIndex() string
+	MarshalToJson() ([]byte, error)
+}
+
+// ModelMarshalToElastic 序列化为ES对象
+func ModelMarshalToElastic(o Item) (elastic.ElasticItem, error) {
+	idx := o.Idx()
+	parrent := o.ElasticIndex()
+
+	body, err := o.MarshalToJson()
+	if err != nil {
+		return nil, err
+	}
+	return elastic.NewElasticItem2(idx, parrent, body)
+}
+
+// MultiMarshalToElastic 序列化为ES对象
+func MultiMarshalToElastic(os ...Item) ([]elastic.ElasticItem, error) {
+	rds := make([]elastic.ElasticItem, 0)
+
+	for _, o := range os {
+		idx := o.Idx()
+		parrent := o.ElasticIndex()
+
+		body, err := o.MarshalToJson()
+		if err != nil {
+			return nil, err
+		}
+
+		rd, err := elastic.NewElasticItem2(idx, parrent, body)
+		if err != nil {
+			return nil, err
+		}
+
+		rds = append(rds, rd)
+	}
+
+	return rds, nil
+}
