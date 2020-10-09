99af524a880601195b7e720efbef6f2e16c08811
wzkun <wzkun1990@126.com>
2020-10-09 17:08:55 +0800

    增加新的错误处理函数

--- consts/Detail.go
+++ consts/Detail.go
@@ -1,6 +1,7 @@
 package consts
 
 const (
+	ItemAlreadyDetail        = "数据已存在"
 	MarshalToElasticDetail   = "转换为es数据出错！"
 	PushMessageToKafkaDetail = "推送数据到kafka出错！"
 )
--- errstring/data.go
+++ errstring/data.go
@@ -1,12 +1,16 @@
 package errstring
 
 import (
+	"strings"
+
 	"code.aliyun.com/bim_backend/zoogoer/gun/errors"
 	"github.com/sirupsen/logrus"
+	"github.com/wzkun/aurora/consts"
 )
 
 const (
 	ItemNotExist            = "record_not_exist.domain.app_error"
+	ItemAlreadyExist        = " record_already_exist.domain.app_error"
 	MarshalToElasticError   = "marshal_to_elastic_error.domain.app_error"
 	PushMessageToKafkaError = "push_message_to_kafka_error.domain.app_error"
 )
@@ -19,11 +23,21 @@ func MakeItemNotExistDetail(id, apiName string) error {
 
 // MakeErrorDebug
 func MakeErrorDebug(apiName string, sort int, err error) {
-	apiName = "--**== " + apiName + "--**== " + string(sort) + " :  "
+	apiName = "--**== " + apiName + "==**-- " + string(sort) + " :  "
 	logrus.Debugln(apiName, err)
 }
 
 // MakeResponseError
 func MakeResponseError(code, detail, apiName string) error {
 	return errors.NewClientErr(nil, code, detail, apiName, nil)
+}
+
+// MakeResponseError2
+func MakeResponseError2(code, detail, apiName string, err error) error {
+	if strings.Contains(err.Error(), "Duplicated") {
+		newdetail := consts.ItemAlreadyDetail + ": " + err.Error()
+		return errors.NewClientErr(nil, ItemAlreadyExist, newdetail, apiName, nil)
+	}
+
+	return MakeResponseError(code, detail, apiName)
 }
